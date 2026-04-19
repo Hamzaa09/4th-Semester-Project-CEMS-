27035e57b644b97c1b77abde2e112248f6825006
Muhammad Hamza <hamza036513@gmail.com>
2026-04-19 13:01:46 +0500

    mongo error

--- server/connection/mongo.connection.js
+++ server/connection/mongo.connection.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 10000,
+      socketTimeoutMS: 45000,
+    });
     console.log("DB connected successfully!");
   } catch (error) {
     console.error("DB connection failed:", error);
