f67ed2eccc7f7e816c27b2c684b988c3b1b9c445
Muhammad Hamza <hamza036513@gmail.com>
2026-04-19 15:03:39 +0500

    arrangements

--- server/server.js
+++ server/server.js
@@ -19,23 +19,17 @@ const port = process.env.PORT;
 connectDB();
 
 //middlewares
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-  })
-);
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//Middlewares
-app.use(errorMiddleware);
-
 app.use("/stripe", paymentRouter);
-app.use("/products/", productRouter);
+app.use("/products", productRouter);
 app.use("/users", userRouter);
 app.use("/orders", orderRouter);
+
+app.use(errorMiddleware); // ✅ always last
 
 export default app;
 
